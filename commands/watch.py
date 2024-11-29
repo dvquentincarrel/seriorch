bf819b7d05e3cd8731aea9b633540adbbeb9035d
Quentin Carrel <dvquentincarrel@gmail.com>
2024-11-29 13:25:13 +0100

    [A] Watch doesn't print anything if process run in background

--- commands/watch.py
+++ commands/watch.py
@@ -1,6 +1,12 @@
 from time import sleep
 from typing import Any
+import re
+import os
 import inotify.adapters
+
+# Some editors recreate an inode for every write. This helps ensure only the correct
+# files trigger the command (temporary files are usually not named like regular files)
+END_EXT_RE = re.compile('\.(xml|css|py|yaml)$')
 
 def watch(command, config, *args, **kwargs):
     """Watch the dir for file writes. Executes the command on file save
@@ -11,12 +17,17 @@ def watch(command, config, *args, **kwargs):
         i = inotify.adapters.Inotify()
         i.add_watch('.')
 
+
         try:
             for event in i.event_gen(yield_nones=False):
                 (_, type_names, path, filename) = event
-                if 'IN_CLOSE_WRITE' in type_names and filename != config['build_name']:
-                    print(f"{filename} modifié")
-                    sleep(0.1) # Certains éditeurs recréent un inode à chaque écriture. Permet d'attendre que le fichier soit recréé
+                if ('IN_CLOSE_WRITE' in type_names 
+                    and filename != config['build_name']
+                    and END_EXT_RE.search(filename)
+                ):
+                    # Process' group and and its stdout's group differ if run in brackground
+                    if os.getpgrp() == os.tcgetpgrp(1):
+                        print(f"{filename} modifié")
                     command(*args, **kwargs)
         except KeyboardInterrupt:
             pass
